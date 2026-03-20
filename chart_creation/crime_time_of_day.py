2dce2f256c92c826f0523f110d0a0673e4656e2d
Mike Covelli <196670706+mcovelli@users.noreply.github.com>
2026-03-20 12:02:31 -0400

    Update crime_time_of_day.py

--- chart_creation/crime_time_of_day.py
+++ chart_creation/crime_time_of_day.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
-path = '/Users/mike/nyc-crime-ai/nyc_crime_clean.parquet'
+# path to data
+base_path = os.path.dirname(__file__)
+path = os.path.join(base_path, 'data', 'nyc_crime_clean.parquet')
 
 df = pd.read_parquet(path)
 
@@ -27,5 +30,5 @@ plt.xlabel('Hour of Day')
 plt.ylabel('Number of Crimes')
 plt.tight_layout()
 plt.subplots_adjust(bottom=0.3)
-plt.savefig('/Users/mike/nyc-crime-ai/crime_time_of_day.jpg')
+plt.savefig(os.path.join(base_path, 'charts', 'crime_time_of_day.jpg')
 plt.show()
