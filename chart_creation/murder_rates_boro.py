3f954bb355ce15909e15d8ac12e89c00bb90561f
Mike Covelli <196670706+mcovelli@users.noreply.github.com>
2026-03-20 12:08:05 -0400

    Update murder_rates_boro.py

--- chart_creation/murder_rates_boro.py
+++ chart_creation/murder_rates_boro.py
@@ -2,9 +2,11 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # path to data
-path = '/Users/mike/nyc-crime-ai/nyc_crime_clean.parquet'
+base_path = os.path.dirname(__file__)
+path = os.path.join(base_path, 'data', 'nyc_crime_clean.parquet')
 
 # load cleaned dataset
 df = pd.read_parquet(path)
@@ -31,5 +33,5 @@ plt.xlabel('Year')
 plt.ylabel('Number of Murders')
 plt.tight_layout()
 plt.subplots_adjust(bottom=0.3)
-plt.savefig("/Users/mike/nyc-crime-ai/murder_rates_plot.jpg")
+plt.savefig(os.path.join(base_path, 'charts', 'murder_rates_plot.jpg'))
 plt.show()
