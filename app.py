48d9966ec138a61fdd64071c13e68f470282eca2
Mike Covelli <196670706+mcovelli@users.noreply.github.com>
2026-03-20 12:54:21 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import streamlit as st
 import ollama
+import os
 
 st.title("NYC Crime Complaints 2006 - 2024")
 st.subheader("Analysis by Michael Covelli")
 
-with open('/Users/mike/nyc-crime-ai/narrative.md', 'r') as f:
+base_path = os.path.dirname(__file__)
+with open(os.path.join(base_path, 'narrative.md'), 'r') as f:
     narrative = f.read()
 st.write(narrative)
-st.image("/Users/mike/nyc-crime-ai/crimes_per_year_plot.jpg")
-st.image("/Users/mike/nyc-crime-ai/crimes_per_year_per_boro_plot.jpg")
-st.image("/Users/mike/nyc-crime-ai/crimes_per_boro_bar.jpg")
-st.image("/Users/mike/nyc-crime-ai/crime_time_of_day.jpg")
-st.image("/Users/mike/nyc-crime-ai/f_susp_m_vic_bar.jpg")
-st.image("/Users/mike/nyc-crime-ai/m_susp_f_vic.jpg")
-st.image("/Users/mike/nyc-crime-ai/murder_rates_plot.jpg")
-st.image("/Users/mike/nyc-crime-ai/offense_by_boro.jpg")
-st.image("/Users/mike/nyc-crime-ai/report_delay.jpg")
-st.image("/Users/mike/nyc-crime-ai/susp_age_group.jpg")
-st.image("/Users/mike/nyc-crime-ai/susp_vic_race.jpg")
-st.image("/Users/mike/nyc-crime-ai/susp_vic_sex.jpg")
+st.image(os.path.join(base_path, 'charts','crimes_per_year_plot.jpg'))
+st.image(os.path.join(base_path, 'charts','crimes_per_year_per_boro_plot.jpg'))
+st.image(os.path.join(base_path, 'charts','crimes_per_boro_bar.jpg'))
+st.image(os.path.join(base_path, 'charts','crime_time_of_day.jpg'))
+st.image(os.path.join(base_path, 'charts','f_susp_m_vic_bar.jpg'))
+st.image(os.path.join(base_path, 'charts','m_susp_f_vic.jpg'))
+st.image(os.path.join(base_path, 'charts','murder_rates_plot.jpg'))
+st.image(os.path.join(base_path, 'charts','offense_by_boro.jpg'))
+st.image(os.path.join(base_path, 'charts','report_delay.jpg'))
+st.image(os.path.join(base_path, 'charts','susp_age_group.jpg'))
+st.image(os.path.join(base_path, 'charts','susp_vic_race.jpg'))
+st.image(os.path.join(base_path, 'charts','susp_vic_sex.jpg'))
